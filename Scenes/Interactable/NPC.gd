169ea2d472f87576027ed18a2daa6e003a5b3956
Olivier Grech <olivier.grech@tutanota.com>
2021-03-04 21:07:03 +0100

    Change dialog box so dialog lines can be set by method call

--- Scenes/Interactable/NPC.gd
+++ Scenes/Interactable/NPC.gd
@@ -3,4 +3,6 @@ extends "res://Scenes/Interactable/Interactable.gd"
 export var dialog_lines := ["Lorem ipsum dolor sit amet.", "Consectetur adipiscing elit, sed do eiusmod tempor incididunt."]
 
 func interact():
+	_dialog_box_instance.set_dialog_lines(dialog_lines)
 	_root.add_child(_dialog_box_instance)
+	_dialog_box_instance.go()
