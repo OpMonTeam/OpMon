0996da967cc4acc0f948f421770887b6f345a41a
Olivier Grech <olivier.grech@tutanota.com>
2021-03-04 21:10:30 +0100

    Change dummy text

--- Scenes/Interactable/NPC.gd
+++ Scenes/Interactable/NPC.gd
@@ -1,6 +1,6 @@
 extends "res://Scenes/Interactable/Interactable.gd"
 
-export var dialog_lines := ["Lorem ipsum dolor sit amet.", "Consectetur adipiscing elit, sed do eiusmod tempor incididunt."]
+export var dialog_lines := ["Fake line 1.", "Fake line 2."]
 
 func interact():
 	_dialog_box_instance = load(PATH_DIALOG_BOX_SCENE).instance()
