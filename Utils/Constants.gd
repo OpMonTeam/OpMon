da3cb3210245da3ef42c6d05a444d57a66dcaf8a
Olivier Grech <olivier.grech@tutanota.com>
2021-03-08 13:33:27 +0100

    Add Constants util script

---
+++ Utils/Constants.gd
@@ -0,0 +1,10 @@
+extends Node
+
+const PATH_CURRENT_SCENE_NODE = "/root/Game/CurrentScene"
+const PATH_USER_INTERFACE_NODE = "/root/Game/UserInterface"
+
+const PATH_CAMERA_SCENE = "res://Scenes/Interface/Camera.tscn"
+const PATH_DIALOG_BOX_SCENE = "res://Scenes/Interface/DialogBox.tscn"
+const PATH_MAIN_MENU_SCENE = "res://Scenes/MainMenu/MainMenu.tscn"
+const PATH_MAP_SCENE = "res://Scenes/Maps/"
+const PATH_PLAYER_SCENE =  "res://Scenes/Player/Player.tscn"
