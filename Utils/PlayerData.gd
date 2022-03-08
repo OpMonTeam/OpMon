191da55fc66afbf59e35c25e68f1401849c46f08
Cyrielle Centori <cyriellecentori@protonmail.com>
2022-03-08 17:24:19 +0100

    Added OpMons and Moves to the game

--- Utils/PlayerData.gd
+++ Utils/PlayerData.gd
@@ -13,7 +13,14 @@ var current_position: Vector2
 func _ready():
 	var tackle = load("res://Data/GodotResources/Moves/Tackle.tres")
 	var growl = load("res://Data/GodotResources/Moves/Growl.tres")
+	var ember = load("res://Data/GodotResources/Moves/Ember.tres")
+	var vine_whip = load("res://Data/GodotResources/Moves/VineWhip.tres")
+	var water_gun = load("res://Data/GodotResources/Moves/WaterGun.tres")
 	var bot_nature = load("res://Data/GodotResources/Natures/Bot.tres")
-	var popmon = OpMon.new("", load("res://Data/GodotResources/Species/Furnurus.tres"), 10,
-	[tackle, growl, null, null], bot_nature)
-	team = OpTeam.new([popmon, null, null, null, null, null])
+	var furnurus = OpMon.new("", load("res://Data/GodotResources/Species/Furnurus.tres"), 10,
+	[tackle, growl, ember, null], bot_nature)
+	var nanolphin = OpMon.new("", load("res://Data/GodotResources/Species/Nanolphin.tres"), 10, 
+	[tackle, growl, water_gun, null], bot_nature)
+	var rosarin = OpMon.new("", load("res://Data/GodotResources/Species/Rosarin.tres"), 10, 
+	[tackle, growl, vine_whip, null], bot_nature)
+	team = OpTeam.new([rosarin, furnurus, nanolphin, null, null, null])
