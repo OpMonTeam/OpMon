9adfdea0f7851a79f32bd7798f7b5ee6caaa3854
Cyrielle <cyriellecentori@protonmail.com>
2023-07-31 10:05:11 +0200

    Redesigned the bag interface

---
+++ Scenes/Interface/Bag/Bag.gd
@@ -0,0 +1,12 @@
+extends Control
+
+# The number of items the list can show. In reality, the list can show one more item,
+# but the twelfth item implies by its placement that there is more items below, so
+# it must not be present at the end of the list.
+const LIST_SIZE := 11
+
+func _ready():
+	pass
+
+func _process(delta):
+	pass
